641a896edff1e28c826edf0eb0424f3df7512f14
nikita <nikita@aveldent.ru>
2023-06-20 15:53:09 +0300

    Добавил новый редактор

--- resources/views/admin/update_view.blade.php
+++ resources/views/admin/update_view.blade.php
@@ -29,30 +29,10 @@
 
 @endsection
 
-@push('css')
-
-    <link href="{{asset('plugins/summernote/summernote-bs4.min.css')}}" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-
-@endpush
-
 @push('scripts')
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
-    <script src="{{asset('plugins/summernote/lang/summernote-ru-RU.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/ckeditor/ckeditor.js')}}"></script>
     <script>
-
-        $(function (){
-
-            $('#exampleInputNameContent').summernote({
-                lang: 'ru-RU',
-                height: 500
-            });
-
-        });
+        CKEDITOR.replace( 'exampleInputNameContent',{height: 800} );
 
     </script>
-
 @endpush
